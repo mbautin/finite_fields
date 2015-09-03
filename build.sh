19d928acb84226d6bc7ca3312132157dba65e055
Mikhail Bautin <mbautin@gmail.com>
2015-09-03 00:59:14 -0700

    Add a build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euxo pipefail
+script_dir=$( cd `dirname $0` && pwd )
+cd "$script_dir"
+git submodule update
+mkdir -p build/googletest
+cd build/googletest
+cmake ../../googletest
+
